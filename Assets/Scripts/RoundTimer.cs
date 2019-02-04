5e7dc0754a09ea4c1dfebed88b847484b27bf12f
Paul Calande <paul.luap09@gmail.com>
2019-02-04 15:42:49 -0500

    Add round timer

---
+++ Assets/Scripts/RoundTimer.cs
@@ -0,0 +1,33 @@
+﻿// Author(s): Paul Calande
+// Fruit Gunch round timer.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundTimer : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Reference to the Text to modify for the timer.")]
+    Text text;
+
+    Timer timerRound;
+
+    private void Start()
+    {
+        timerRound = new Timer(180.0f, TimerRound_Finished, false);
+        timerRound.Run();
+    }
+
+    private void FixedUpdate()
+    {
+        timerRound.Tick(Time.deltaTime);
+        text.text = UtilString.DigitalTime(timerRound.GetSecondsRemaining());
+    }
+
+    private void TimerRound_Finished(float secondsOverflow)
+    {
+        // End of game.
+    }
+}
