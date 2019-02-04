ab2f9590a4e77015bc3425d46186bf87bda5b9bd
Paul Calande <paul.luap09@gmail.com>
2019-02-04 10:47:20 -0500

    Start implementing Twitch integration

---
+++ Assets/Scripts/Secrets.cs
@@ -0,0 +1,15 @@
+﻿// Author(s): Paul Calande
+// Authorization information for Twitch.
+// HIGHLY CONFIDENTIAL! DON'T SHOW THIS TO ANYONE ELSE!
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Secrets
+{
+    public const string clientID = "trqp22swngokhmqvrawv8fyqyq3xyx";
+    public const string clientSecret = "2r2rxp8ts7z0n6po2bzq83ndo847vk";
+    public const string accessToken = "ax65uzdjest0czjwjdgwbtbvnkb6zp";
+    public const string refreshToken = "3ijiklk2xc2e9izd31cuogsg61tg8lniieq3w4h4y8zj65l973";
+}
---
+++ Assets/Scripts/TwitchClient.cs
@@ -0,0 +1,48 @@
+﻿// Author(s): Paul Calande
+// A script for managing the Twitch client.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TwitchLib.Unity;
+using TwitchLib.Client.Models;
+using System;
+using TwitchLib.Client.Events;
+
+public class TwitchClient : MonoBehaviour
+{
+    public Client client;
+    string channelName = "toomuchfanservice";
+
+    private void Start()
+    {
+        // Make sure the game is always running.
+        Application.runInBackground = true;
+
+        // Set up the bot.
+        ConnectionCredentials credentials = new ConnectionCredentials(
+            "theonetruebeetbot", Secrets.accessToken);
+        client = new Client();
+        client.Initialize(credentials, channelName);
+
+        // Subscribe to the relevant events here.
+        client.OnMessageReceived += MessageReceived;
+
+        // Connect to the channel.
+        client.Connect();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            client.SendMessage(client.JoinedChannels[0], "Why hello!");
+        }
+    }
+
+    // Callback method for receiving a chat message.
+    private void MessageReceived(object sender, OnMessageReceivedArgs e)
+    {
+        Debug.Log(e.ChatMessage.Username + ": " + e.ChatMessage.Message);
+    }
+}
